f96c33758238bc9c0c8af8c2181e6bbf578bbc6a
Aditi Kesarwani <62420198+aditi1042003@users.noreply.github.com>
2023-11-16 23:12:39 +0530

    added prizes page
    initial design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Prizes from './pages/Prizes';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/prizes" element={<Prizes />} />
+
       </Routes>
     </Router>
 
