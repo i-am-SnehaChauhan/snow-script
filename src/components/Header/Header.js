e1761e8e8d73794e73b93ecaed6f016b80ff84e7
Kirti Aggarwal <kirtiaggarwal@Kirtis-MacBook-Air.local>
2023-12-15 23:29:34 +0530

    header fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
-import "./Header.css"
+import "./Header.css";
+import logo from "../../assets/logogdsc.png"
 function Header()
 {
     return(
@@ -6,15 +7,18 @@ function Header()
         <header class="header">
             <nav>
                 <div class="logo">
-                <a href="index.html">Snow Script Winter Of Code</a>
+                <a href="index.html"><img src={logo} className="logogdsc"></img></a>
                 </div>
                 <input type="checkbox" id="menu-toggle"/>
                 <label for="menu-toggle" class="menu-icon">&#9776;</label>
                 <ul class="menu">
                 <li><a href="/">Home</a></li>
                 <li><a href="/projects">Projects</a></li>
-                <li><a href="/events">Events</a></li>
+                <li><a href="/">Timeline</a></li>
+                <li><a href="/">How to Contribute</a></li>
                 <li><a href="/prizes">Prizes</a></li>
+                <li><a href="/prizes" className="buttonheader">Register</a></li>
+                <li><a href="/prizes" className="buttonheader">References</a></li>
                 </ul>
             </nav>
         </header>
