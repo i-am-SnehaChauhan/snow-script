58fb8407f00bce2b6e420d3386dcbb3c34c0d472
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-12-22 01:33:47 +0530

    header fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,15 +7,15 @@ function Header()
         <header class="header">
             <nav>
                 <div class="logo">
-                <a href="index.html"><img src={logo} className="logogdsc"></img></a>
+                <a href="/"><img src={logo} className="logogdsc"></img></a>
                 </div>
                 <input type="checkbox" id="menu-toggle"/>
                 <label for="menu-toggle" class="menu-icon">&#9776;</label>
                 <ul class="menu">
                 <li><a href="/">Home</a></li>
                 <li><a href="/projects">Projects</a></li>
-                <li><a href="/">Timeline</a></li>
                 <li><a href="/">How to Contribute</a></li>
+                <li><a href="/">Timeline</a></li>
                 <li><a href="/prizes">Prizes</a></li>
                 <li><a href="/prizes" className="buttonheader">Register</a></li>
                 <li><a href="/prizes" className="buttonheader">References</a></li>
