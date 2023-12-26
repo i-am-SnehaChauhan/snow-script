aaeb969b30d009789805ca9e711b8360c09c1c60
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-12-27 01:05:43 +0530

    updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,66 @@
 import "./Header.css";
-import logo from "../../assets/logogdsc.png"
-function Header()
-{
-    return(
-        <>
-        <header class="header">
-            <nav>
-                <div class="logo">
-                <a href="/"><img src={logo} className="logogdsc"></img></a>
-                </div>
-                <input type="checkbox" id="menu-toggle"/>
-                <label for="menu-toggle" class="menu-icon">&#9776;</label>
-                <ul class="menu">
-                <li><a href="/">Home</a></li>
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/">How to Contribute</a></li>
-                <li><a href="/">Timeline</a></li>
-                <li><a href="/prizes">Prizes</a></li>
-                <li><a href="https://forms.gle/7GCJFgDamBqM5hxFA" className="buttonheader">Register</a></li>
-                <li><a href="https://drive.google.com/file/d/1jhaN9rbHmW5J9TpgEFlNvS9PPxtskbGE/view?usp=drive_link" className="buttonheader">References</a></li>
-                <li><a href="https://discord.com/invite/SzpZBPXPVA"><i class="fab fa-discord"></i></a></li>
-                </ul>
-            </nav>
-        </header>
-        </>
-    )
+import logo from "../../assets/logogdsc.png";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+function Header() {
+  return (
+    <>
+      <header class="header">
+        <nav>
+          <div class="logo">
+            <a href="/">
+              <img src={logo} className="logogdsc"></img>
+            </a>
+          </div>
+          <input type="checkbox" id="menu-toggle" />
+          <label for="menu-toggle" class="menu-icon">
+            &#9776;
+          </label>
+          <ul class="menu">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/projects">Projects</a>
+            </li>
+            {/* </li>
+            <li>
+              <a href="/">How to Contribute</a>
+            </li>
+            <Link to="timeline" smooth={true} duration={500}>
+              Scroll to Component
+            </Link>
+            <li>
+              <a href="/">Timeline</a>
+            </li> */}
+            <li>
+              <a href="/prizes">Prizes</a>
+            </li>
+            <li>
+              <a
+                href="https://forms.gle/7GCJFgDamBqM5hxFA"
+                className="buttonheader"
+              >
+                Register
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://drive.google.com/file/d/1jhaN9rbHmW5J9TpgEFlNvS9PPxtskbGE/view?usp=drive_link"
+                className="buttonheader"
+              >
+                References
+              </a>
+            </li>
+            <li>
+              <a href="https://discord.com/invite/SzpZBPXPVA">
+                <i class="fab fa-discord"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 export default Header;
--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -2,173 +2,64 @@ import React from "react";
 import Timeline from "../assets/Timeline.png";
 import { Box } from "@mui/system";
 
-
-
 const TimeLine = () => {
   return (
     <>
-   <div
-     style={{
-      
-        margin: '2px', // Adjust the value as needed
-     }}
-   >
-    <h1 
-        className="mainHeading"
+      <div
+        id="timeline"
         style={{
-            display: 'block',
-            maxWidth: '100%',
-            textAlign: 'center',
-            color: 'white',
-            fontWeight: '900',
-            fontFamily: 'sans-serif',
-            marginTop: '40px',
-            marginBottom: '10px',
-            padding: '0 50px',
-            
-        }}
-    >
-        TimeLine
-    </h1>
-</div>
-
-      
-      {/* <div
-        className="prizes"
-        style={{
-          backgroundImage: `url(${''})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          marginTop: "200px",
-          margin: "20px",
+          margin: "2px", // Adjust the value as needed
         }}
       >
-        <VerticalTimeline lineColor="#12486B">
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-             date="2011 - present"
-             dateClassName="date-color" 
-             contentStyle={{ background: 'rgb(18, 72, 107)', color: '#fff' }}
-             contentArrowStyle={{ borderRight: '7px solid  rgb(18, 72, 107)' }}
-             iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-             icon={<FaRegSnowflake />}
-            
-          >
-            <h3 className="vertical-timeline-element-title">Top 3</h3>
-
-            <p>
-              <ul className="prizeList">
-                <li>Special Prize</li>
-                <li>Shoutouts on Social Media handles</li>
-                <li>Swag Kits and lot of exciting goodies</li>
-                <li>Certificate of appreciation</li>
-              </ul>
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: 'rgb(18, 72, 107)', color: '#fff' }}
-             dateStyle={{ color: '#12486B' }}
-             contentArrowStyle={{ borderRight: '7px solid  rgb(18, 72, 107)' }}
-             date="2011 - present"
-             iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-             icon={<FaRegSnowflake />}
-          >
-            <h3 className="vertical-timeline-element-title">Top 5</h3>
-        
-            <p>
-              <ul className="prizeList">
-                <li>Surprise Gift</li>
-                <li>Shoutouts on Social Media handles</li>
-                <li>Swag Kits and lot of exciting goodies</li>
-                <li>Certificate of appreciation</li>
-              </ul>
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: 'rgb(18, 72, 107)', color: '#fff' }}
-             dateStyle={{ color: '#12486B' }}
-             contentArrowStyle={{ borderRight: '7px solid  rgb(18, 72, 107)' }}
-             date="2011 - present"
-             iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-             icon={<FaRegSnowflake />}
-          >
-            <h3 className="vertical-timeline-element-title">Top 10</h3>
-            
-            <p>
-              <ul className="prizeList">
-                <li>Shoutouts on Social Media handles</li>
-                <li>Swag Kits and lot of exciting goodies</li>
-                <li>Certificate of appreciation</li>
-              </ul>
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<FaRegSnowflake />}
-          >
-            <h3 className="vertical-timeline-element-title">Top 25</h3>
-            
-            <p>
-              <ul className="prizeList">
-                <li>Swag Kits and lot of exciting goodies</li>
-                <li>Certificate of appreciation</li>
-              </ul>
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            contentStyle={{ background: 'rgb(18, 72, 107)', color: '#fff' }}
-             dateStyle={{ color: '#12486B' }}
-             contentArrowStyle={{ borderRight: '7px solid  rgb(18, 72, 107)' }}
-             date="2011 - present"
-             iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-             icon={<FaRegSnowflake />}
-          >
-            <h3 className="vertical-timeline-element-title">
-              ALL Contributors
-            </h3>
-            
-            <p>
-              <ul className="prizeList">
-                <li>
-                  Certificate of appreciation for your First successful Pull
-                  Request
-                </li>
-              </ul>
-            </p>
-          </VerticalTimelineElement>
-        </VerticalTimeline>
-      </div>  */}
+        <h1
+          className="mainHeading"
+          style={{
+            display: "block",
+            maxWidth: "100%",
+            textAlign: "center",
+            color: "white",
+            fontWeight: "900",
+            fontFamily: "sans-serif",
+            marginTop: "40px",
+            marginBottom: "10px",
+            padding: "0 50px", // Default padding for medium and large screens
+          }}
+        >
+          TimeLine
+        </h1>
+      </div>
 
       <Box
         style={{
-          padding: "10px 80px",
+          position: "relative",
+          padding: "10px 30px", // Default padding for small screens
           display: "flex",
-          sx: {
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundSize: "cover",
-            backgroundBlendMode: "overlay",
-            color: "white",
-            filter: "drop-shadow(0px 7px 4px rgba(0, 0, 0, 0.25))",
-            overflow: "hidden",
-            zIndex: 0,
-            width: "a",
-            height: "auto",
-          },
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundSize: "cover",
+          backgroundBlendMode: "overlay",
+          color: "white",
+          filter: "drop-shadow(0px 7px 4px rgba(0, 0, 0, 0.25))",
+          overflow: "hidden",
+          zIndex: 0,
+          width: "100%",
+          height: "auto",
         }}
       >
-       <img src={Timeline} alt="timeline" style={{ width: "100%", justifyContent:'center' }} />
-        
-     
-
+        <img
+          src={Timeline}
+          alt="timeline"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            justifyContent: "center",
+          }}
+        />
       </Box>
     </>
-    );
-}
+  );
+};
 
 export default TimeLine;
