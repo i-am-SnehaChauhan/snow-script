9e95fd80b69d554eebc9789022a1f6767c83297f
Akanchha <sakanchha111@gmail.com>
2023-12-27 08:58:18 +0530

    Questions added

--- src/components/FAQs/FAQs.js
+++ src/components/FAQs/FAQs.js
@@ -4,23 +4,25 @@ import "./FAQs.css";
 const faqs = [
     {
         id: 1,
-        question: "What is Lorem Ipsum?",
-        answer: `Lorem Ipsum is simply dummy answer of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy answer ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`
+        question: "What is SWOC",
+        answer: `SWOC, organized by GDSC IGDTUW, is a month-long open-source event aimed at inspiring and guiding passionate innovators. Participants can contribute to diverse projects, receiving mentorship to enhance their technical skills for impactful solutions, from the comfort of their homes.`
     },
     {
         id: 2,
-        question: "Where does it come from?",
-        answer: `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. `
+        question: "Who can contribute?",
+        answer: `Everyone irrespective of age, gender and technical experience is welcome!`
+        
     },
     {
         id: 3,
-        question: "Why do we use it?",
-        answer: `Contrary to popular belief, Lorem Ipsum is not simply random answer. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature,`
+        question: "Do I need past experience?",
+        answer: `No, you don't need any past experience. You can contribute even if you are an absolute beginner. `
+        
     },
     {
         id: 4,
-        question: "Where can I get some?",
-        answer: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.`
+        question: "Where will GDSC Snowscript Winter Of Code be held?",
+        answer: `You can contribute to any of the existing projects online on GitHub from the comfort of your home.`
     }
 ]
 
